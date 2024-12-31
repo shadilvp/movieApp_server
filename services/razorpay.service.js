f763443fa86b162da9daafe3cbea2699de802ae0
shadilvp <shadilx8@gmail.com>
2024-12-31 08:47:59 +0530

    completed

---
+++ services/razorpay.service.js
@@ -0,0 +1,11 @@
+import Razorpay from "razorpay";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const razorpayInstence = new Razorpay({
+    key_id : process.env.RAZORPAY_KEY_ID,
+    key_secret : process.env.RAZORPAY_KEY_SECRET 
+})
+
+export default razorpayInstence ;
