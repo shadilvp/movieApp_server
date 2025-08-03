b34662d43995ecbd4bb381c5abd7c9c71ad4cf43
shadilvp <shadilx8@gmail.com>
2025-08-03 11:49:29 +0530

    changed the url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://movie-app-phi-woad.vercel.app",
   credentials: true,
 }));
 
